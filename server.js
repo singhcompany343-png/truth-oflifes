0868bd71aee0ab2f35bb2d006304e6ef87b1299d
singhcompany343-png <singh.company343@gmail.com>
2026-09-10 05:44:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET || "CHANGE_THIS_SECRET";
