7636480619812cbd213f9c79f10f47d1594cda1f
singhcompany343-png <singh.company343@gmail.com>
2026-09-11 11:40:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1121,7 +1121,6 @@ app.get("/api/users", adminAuth, async (req, res) => {
       SELECT
         id,
         instagram_username,
-        email,
         created_at
       FROM users
       ORDER BY created_at DESC
